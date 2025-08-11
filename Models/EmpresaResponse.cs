304db93c04a6f6e564638d64237731d2a36ffa9f
DanielCoronel <danicoronel2022@hotmail.com>
2025-08-11 11:01:59 -0300

    Primer commit: Proyecto web drr- internacional

---
+++ Models/EmpresaResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace DRR_PRESENTATION.Models
+{
+    public class EmpresaResponse
+    {
+        public string Status { get; set; } = string.Empty;
+        public List<Empresa> Data { get; set; } = new();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+using DRR_PRESENTATION;
+using DRR_PRESENTATION.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<EmpresaService>();
+
+await builder.Build().RunAsync();
+
