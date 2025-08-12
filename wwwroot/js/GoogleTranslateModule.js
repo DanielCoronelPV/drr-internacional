6574832e5164911d8800b62f11b4efa0c1befad6
Luisito Salvarezza <desarrollo1@drrsystemas.com>
2025-08-12 09:41:38 -0300

    componente - Traductos.

---
+++ wwwroot/js/GoogleTranslateModule.js
@@ -0,0 +1,30 @@
+﻿
+export function loadGoogleTranslate() {
+    return new Promise((resolve, reject) => {
+        if (window.google?.translate?.TranslateElement) {
+            init();
+            resolve();
+            return;
+        }
+
+        const script = document.createElement('script');
+        script.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateCallback';
+        script.async = true;
+        script.onerror = reject;
+
+        window.googleTranslateCallback = () => {
+            init();
+            resolve();
+        };
+
+        document.head.appendChild(script);
+
+        function init() {
+            new google.translate.TranslateElement({
+                pageLanguage: 'es',
+                includedLanguages: 'es,en,pt,it,fr,de,zh-CN,zh-TW',
+                layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+            }, 'google_translate_element');
+        }
+    });
+}
